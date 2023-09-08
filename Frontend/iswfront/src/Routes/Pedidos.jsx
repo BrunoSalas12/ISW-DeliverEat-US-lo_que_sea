6bc23e2adbb76900357cdc2d15497cdb92e462f9
juaniandrada23 <juanandrada_07_@hotmail.com>
2023-09-08 01:54:56 -0300

    Agg front para pagos y fecha de recepcion

--- Frontend/iswfront/src/Routes/Pedidos.jsx
+++ Frontend/iswfront/src/Routes/Pedidos.jsx
@@ -6,6 +6,7 @@ import { FiUpload } from "react-icons/fi";
 import Select from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
 import TextField from '@mui/material/TextField';
+import Checkbox from '@mui/material/Checkbox';
 import '../Styles/pedidos.css'
 
 const Pedidos = () => {
@@ -19,10 +20,26 @@ const Pedidos = () => {
   const [numeroEntrega, setNumeroEntrega] = useState("");
   const [referenciaEntrega, setReferenciaEntrega] = useState("");
 
+  const [fecha, setFecha] = useState("");
+  const [hora, setHora] = useState("");
+  const [seleccionarFecha, setSeleccionarFecha] = useState(false);
+  const [loAntesPosible, setLoAntesPosible] = useState(true);
+
+  const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
+
+  const handleSeleccionarFechaChange = () => {
+    setSeleccionarFecha(!seleccionarFecha);
+    setLoAntesPosible(false);
+  };
+
+  const handleLoAntesPosibleChange = () => {
+    setLoAntesPosible(!loAntesPosible);
+    setSeleccionarFecha(false); 
+  };
+
   return (
     <div>
       <Navbar/>
-        <div>
 
         <h3 className='py-3 pl-4 textos'>Nuevo pedido</h3>
         <div className='detallePedido'>
@@ -31,8 +48,8 @@ const Pedidos = () => {
 
             <Grid item xs={12} lg={5} className='grilla1'>
               <div className='div1'>
-                <label className='mt-10 mb-2'>Productos a enviar:</label>
-                <button className="mb-10 rounded-full botonfotos py-2 px-4 flex justify-center flex-col items-center">Subir fotos<FiUpload/></button>              
+                <label className='mt-10 mb-2 font-bold'>Productos a enviar:</label>
+                <button className="mb-2 rounded-full botonfotos py-2 px-4 flex justify-center flex-col items-center">Subir fotos<FiUpload/></button>              
               </div>
             </Grid>
 
@@ -45,7 +62,7 @@ const Pedidos = () => {
           <h3 className='py-3 pl-4 textos'>Datos de envío</h3>
           <div className='datosDeEnvio'>
               <div className='selectCiudad ml-10 my-5'>
-                <label>Ciudad:</label>
+                <label className='font-bold'>Ciudad:</label>
                 <Select className='ml-3' labelId="demo-simple-select-label" id="demo-simple-select" value={ciudad} onChange={(e) => setCiudadSeleccionada(e.target.value)}>
                   <MenuItem value={"Villa Carlos Paz"}>Villa Carlos Paz</MenuItem>
                   <MenuItem value={"Santa Rosa de Calamuchita"}>Santa Rosa de Calamuchita</MenuItem>
@@ -60,12 +77,12 @@ const Pedidos = () => {
           <div className='detallesDeComercio'>
             <Grid container>
               <Grid item xs={12} lg={5}>
-                <div className='flex flex-col mx-10 mt-2 mb-4'>
-                  <label className='lg:mr-2'>Calle:</label>
+                <div className='flex flex-col mx-10 mt-2 mb-4 gap-1'>
+                  <label className='font-bold lg:mr-2'>Calle:</label>
                   <TextField id="filled-search" label="Calle" type="search" variant="filled" value={calleComercio} onChange={(e) => setCalleComercio(e.target.value)}/>
-                  <label className='lg:mr-2'>Nro:</label>
+                  <label className='font-bold lg:mr-2'>Nro:</label>
                   <TextField id="filled-search" label="Nro" type="search" variant="filled" value={numeroComercio} onChange={(e) => setNumeroComercio(e.target.value)}/>
-                  <label className='lg:mr-2'>Referencia:</label>
+                  <label className='font-bold lg:mr-2'>Referencia:</label>
                   <TextField id="filled-search" label="Referencia" type="search" variant="filled" value={referenciaComercio} onChange={(e) => setReferenciaComercio(e.target.value)}/>
                 </div>
               </Grid>
@@ -80,12 +97,12 @@ const Pedidos = () => {
           <div className='detallesDeEntrega'>
             <Grid container>
               <Grid item xs={12} lg={5}>
-                <div className='flex flex-col mx-10 mt-2 mb-4'>
-                  <label className='lg:mr-2'>Calle:</label>
+                <div className='flex flex-col mx-10 mt-2 mb-4 gap-1'>
+                  <label className='font-bold lg:mr-2'>Calle:</label>
                   <TextField id="filled-search" label="Calle" type="search" variant="filled" value={calleEntrega} onChange={(e) => setCalleEntrega(e.target.value)}/>
-                  <label className='lg:mr-2'>Nro:</label>
+                  <label className='font-bold lg:mr-2'>Nro:</label>
                   <TextField id="filled-search" label="Nro" type="search" variant="filled" value={numeroEntrega} onChange={(e) => setNumeroEntrega(e.target.value)}/>
-                  <label className='lg:mr-2'>Referencia:</label>
+                  <label className='font-bold lg:mr-2'>Referencia:</label>
                   <TextField id="filled-search" label="Referencia" type="search" variant="filled" value={referenciaEntrega} onChange={(e) => setReferenciaEntrega(e.target.value)}/>
                 </div>
               </Grid>
@@ -96,9 +113,46 @@ const Pedidos = () => {
             </Grid>
           </div>
 
+          <h3 className='py-3 pl-4 textos'>Datos de pago</h3>
+          <Grid container>
+            <Grid items xs={12} lg={5}>
+              <div className='mx-10 mt-2 datosDePago'>
+                <label className='font-bold'>Total a pagar: $...</label>
+              </div>
+              <div className='mb-4 botonesPagos'>
+                  <label>Forma de pago</label>
+                  <div className='direccionbotonesPago'>
+                    <button className='rounded-full flex flex-col botonfotos py-2 px-4'>En efectivo</button>
+                    <button className='rounded-full flex flex-col botonfotos py-2 px-4'>Con tarjeta</button>
+                  </div>
+              </div>
+            </Grid>
+            <Grid items xs={12} lg={7}>
+              <div className='interfazPago'>
+                <h1>Poner interfaces para el pago</h1>
+              </div>
+            </Grid>
+          </Grid>
+
+          <h3 className='py-3 pl-4 textos'>Fecha de recepción</h3>
+          <div className="fechaRecepcion my-2">
+            <div className='labelfechas'>
+
+            <Checkbox {...label} checked={loAntesPosible} onChange={handleLoAntesPosibleChange}/>
+            <label>Lo antes posible</label>
+            <Checkbox {...label} checked={seleccionarFecha} onChange={handleSeleccionarFechaChange}/>
+            <label>Seleccionar fecha y hora</label>
+            </div>
+
+              {seleccionarFecha && (
+              <div className="inputsFechaHora">
+                <TextField id="fecha" label="Fecha" type="date" variant="outlined" fullWidth InputLabelProps={{ shrink: true }} value={fecha} onChange={(e) => setFecha(e.target.value)}/>
+                <TextField id="hora" label="Hora" type="time" variant="outlined" fullWidth InputLabelProps={{ shrink: true}} value={hora} onChange={(e) => setHora(e.target.value)}/>
+              </div>
+              )}            
+          </div>
         </div>
 
-        </div>
       <Footer/>
     </div>
   )
